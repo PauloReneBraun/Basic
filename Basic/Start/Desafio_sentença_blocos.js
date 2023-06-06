1654eee7a7616a79eb013ce487278a5158386ff6
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-06-06 09:10:23 -0300

    Add files via upload

---
+++ Basic/Start/Desafio_sentença_blocos.js
@@ -0,0 +1,15 @@
+{
+    Console.log(1);
+    Console.log(3);
+    Console.log(5);
+    Console.log(7);
+    Console.log(9);
+}
+
+{
+    Console.log(2);
+    Console.log(4);
+    Console.log(6);
+    Console.log(8);
+    Console.log(10);
+}
