443ec870296d21999dd61506df3a6bfcb0280c7d
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-06-20 10:32:49 -0300

    Add files via upload

--- Basic/Start/dados_3.js
+++ Basic/Start/dados_3.js
@@ -4,3 +4,9 @@ console.log(typeof idade);
 
 let salario = 4578.32;
 console.log(typeof salario);
+
+let estaChovendo = true
+console.log(typeof estaChovendo);
+
+console.log(typeof "teste");
+console.log(typeof 'teste')
