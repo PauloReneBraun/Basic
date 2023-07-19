1139e617d0c4b0e5b2a28229ab11f41a87ba87d6
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-07-19 12:43:38 -0300

    Add files via upload

--- Basic/Operadores/atribuicao.js
+++ Basic/Operadores/atribuicao.js
@@ -3,6 +3,16 @@ x = x + 6;
 x = x - 1;
 
 let y = 20;
-y = 10;
+y += 10;
+y -= 2;
+y *= 2;
+y /= 4;
+y %= 3;
 
 console.log(x);
+console.log
+
+let w = 'texto';
+w += ', mais texto';
+
+console.log(w)
