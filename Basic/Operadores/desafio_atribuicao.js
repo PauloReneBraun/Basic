09ac23d02612f55440ad348a0c5db2029f358008
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-07-20 09:08:25 -0300

    Add files via upload

---
+++ Basic/Operadores/desafio_atribuicao.js
@@ -0,0 +1,8 @@
+let texto = 'Lista de Aprovados';
+texto += '==================';
+texto += '1. Ana Silva';
+texto += '2. Pedro Rocha';
+texto += '3. Guilherme Molinari';
+texto += '4 Rebeca França';
+
+console.log(texto);
