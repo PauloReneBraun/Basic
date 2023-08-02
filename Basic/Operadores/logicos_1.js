57996191d78f18009d65c32dd3734e1cbf8e49d4
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-08-02 11:08:01 -0300

    Add files via upload

--- Basic/Operadores/logicos_1.js
+++ Basic/Operadores/logicos_1.js
@@ -1,6 +1,9 @@
 let HaveMoney = true;
 let verytangled = true;
+let carronthegarrage = true;
 
-let resultE = 'Go to mall' ;
+let resultE = 'Go to mall?' ;
 resultE += HaveMoney && verytangled;
 console.log(resultE);
+
+let resultOU = '#2 - Go to mall?'
