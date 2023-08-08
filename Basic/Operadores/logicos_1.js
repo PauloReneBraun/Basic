a3e6bbd2f07b433a4a367e0ef5f2ee3f77de4a83
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-08-08 09:29:43 -0300

    Add files via upload

--- Basic/Operadores/logicos_1.js
+++ Basic/Operadores/logicos_1.js
@@ -9,3 +9,11 @@ console.log(resultE);
 let resultOU = '#2 - (OR) Go to mall?'
 resultOU += sunnyDay || carronthegarrage;
 console.log(resultOU);
+
+console.log(true !== true);
+console.log(true !== false);
+console.log(false!== true);
+console.log(false !== false);
+
+console.log('Not True:' + !true);
+console.log('not false:' + !false);
