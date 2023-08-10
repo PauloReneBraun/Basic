e0f566949283920f1c772b4ea3e3a071bb2e5c75
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-08-10 09:30:50 -0300

    Add files via upload

--- Basic/Operadores/logicos_2.js
+++ Basic/Operadores/logicos_2.js
@@ -1,2 +1,12 @@
 const t1 = true;
 const t2 = true;
+
+let buyTv50
+console.log('vamos comprar a TV 50"?', ); //AND
+console.log('vamos comprar a TV 32"?', t1 !== t2); // XOR
+
+let tomarSorvete = t1 || t2;
+console.log('Vamos comprar sorvete?', tomarSorvete); // OR
+
+let ficarEmCasa = !tomarSorvete;
+console.log('vamosficar em casa?', ficarEmCasa); //NOT
