85dc4c0b176960bc174d2e5bc9d173b98e195dc5
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-08-10 09:51:29 -0300

    Add files via upload

--- Basic/Operadores/logicos_2.js
+++ Basic/Operadores/logicos_2.js
@@ -1,12 +1,14 @@
 const t1 = true;
 const t2 = true;
 
-let buyTv50
-console.log('vamos comprar a TV 50"?', ); //AND
-console.log('vamos comprar a TV 32"?', t1 !== t2); // XOR
+let buyTv50 = t1 && t2; // AND
+console.log('vamos comprar a TV 50"?', );
 
-let tomarSorvete = t1 || t2;
-console.log('Vamos comprar sorvete?', tomarSorvete); // OR
+let buyTv32 = t1 !== t2; //XOR
+console.log('vamos comprar a TV 32"?', t1 !== t2); 
 
-let ficarEmCasa = !tomarSorvete;
-console.log('vamosficar em casa?', ficarEmCasa); //NOT
+let tomarSorvete = t1 || t2; // OR
+console.log('Vamos comprar sorvete?', tomarSorvete); 
+
+let ficarEmCasa = !tomarSorvete; //NOT
+console.log('vamosficar em casa?', ficarEmCasa); 
