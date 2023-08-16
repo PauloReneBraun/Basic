99a53a0c7eda8d38857293b574b5e00edf77ab37
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-08-16 10:46:00 -0300

    Add files via upload

--- Basic/Controle/if_2.js
+++ Basic/Controle/if_2.js
@@ -1,11 +1,11 @@
 const nota = 8;
 const bomComportamento = false;
 
-if (nota >=7)
+if (nota >=7 && bomComportamento)
     console.log('Parabéns!');
 
 
-if(nota < 7)
+if(nota < 7 || !bomComportamento)
     console.log('Chato né!');
 
 console.log('Fim!')
