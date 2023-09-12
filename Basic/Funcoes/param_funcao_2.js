8b6a408733655388ef11d0b14a2a21ec8406e6a9
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-09-12 11:42:21 -0300

    Add files via upload

---
+++ Basic/Funcoes/param_funcao_2.js
@@ -0,0 +1,12 @@
+function somar (a, b, c, d) {
+    return a + b + c + d;
+}
+
+console.log(somar(1, 2, 3, 4, 5, 6));
+console.log(somar(1, 2, 3, 4, 5));
+console.log(somar(1, 2, 3));
+console.log(somar(1, 2));
+console.log(somar(1));
+console.log(somar());
+
+console,log('Fim');
