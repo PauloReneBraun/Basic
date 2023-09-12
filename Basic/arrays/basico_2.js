e5cff3e83ccc1cf043eba0eedc9f1d959e7816d6
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-09-12 15:41:01 -0300

    Add files via upload

---
+++ Basic/arrays/basico_2.js
@@ -0,0 +1,10 @@
+const nomes = [];
+
+nomes[0] = "Gustavo";
+nomes[1] = "Rafa";
+nomes[2] = "Juliano";
+nomes[3] = "Maria";
+nomes[4] = "giselle";
+nomes[5] = "Paulo";
+
+console.log(nomes);
---
+++ Basic/arrays/for_1.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 626, 4, 5, 6, 15, 51];
+
+for(let i = 0; 1 < numeros.length; i++ ) {
+    console.log(numeros[i]);
+}
+
+for(let i = 0; 1 < numeros.length; i+= 2 ) {
+    console.log(numeros[i]);
+}
