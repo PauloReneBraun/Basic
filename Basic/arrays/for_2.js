f6f70478f275f194479ad841682b6aaa0be42a5d
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2023-09-12 15:47:52 -0300

    Add files via upload

---
+++ Basic/arrays/for_2.js
@@ -0,0 +1,18 @@
+const notas = [7, 8, 5, 10 , 9 , 8 , 8];
+
+let = valores = '';
+
+// foreach
+for(let nota of notas) {
+    valores += nota + '';
+}
+
+console.log(valores);
+
+let indices = '';
+
+for(let indice of notas) {
+    indices += indice+ '';
+}
+
+console.log(indices);
